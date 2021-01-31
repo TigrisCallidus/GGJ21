7e7bc25225f2a8d8a7674757f199ff67a208031e
SashaYak <45936832+SashaYak@users.noreply.github.com>
2021-01-31 15:08:30 +0100

    UI update

---
+++ Assets/Code/Buttons_Sounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class Buttons_Sounds : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    
+    public AudioClip[] buttonSounds;
+
+    private void Awake()
+    {
+        if (!GetComponent<AudioSource>()) gameObject.AddComponent<AudioSource>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        int rand = Random.Range(0, buttonSounds.Length);
+        GetComponent<AudioSource>().PlayOneShot(buttonSounds[rand]);
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        int rand = Random.Range(0, buttonSounds.Length);
+        GetComponent<AudioSource>().PlayOneShot(buttonSounds[rand]);
+    }
+}
+
+

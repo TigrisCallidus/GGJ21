efc2f9bf9fe0e4a8e0a1176c23791b711c414f06
SashaYak <45936832+SashaYak@users.noreply.github.com>
2021-01-31 16:05:13 +0100

    Hungry sounds

--- Assets/Code/UI_Logic.cs
+++ Assets/Code/UI_Logic.cs
@@ -18,6 +18,17 @@ public class UI_Logic : MonoBehaviour
     public TextMeshProUGUI ropeLength;
     private bool onNoRope = false;
 
+    public RectTransform hungerBarRecTrans;
+    public enum HungerStage {full, m, s, xs }
+    HungerStage currentHungerStage = HungerStage.full;
+
+    public Animator magen;
+    public AudioClip hunger1;
+    public AudioClip hunger2;
+    public AudioClip hunger3;
+
+
+
     public GameObject inGameMenu;
     public GameObject winMenu;
     public GameObject loseMenu;
@@ -52,6 +63,48 @@ public class UI_Logic : MonoBehaviour
         float rectX = 100 - ((float)MazeController.CurrentZweifel / (float)MazeController.MaxZweifel * 100f);
         chipsBarRecTrans.sizeDelta = new Vector2(rectX, chipsBarRecTrans.sizeDelta.y);
 
+
+        float rectYHunger = 100f - (MazeController.FullMeter * 100f);
+        hungerBarRecTrans.sizeDelta = new Vector2(hungerBarRecTrans.sizeDelta.x, rectYHunger);
+
+        if (MazeController.FullMeter > 0.5f)
+        {
+            currentHungerStage = HungerStage.full;
+        }
+
+
+        if (MazeController.FullMeter > 0.3f && MazeController.FullMeter < 0.5f)
+        {
+            if (currentHungerStage != HungerStage.m)
+            {
+                magen.SetTrigger("Jitter");
+                currentHungerStage = HungerStage.m;
+                GetComponent<AudioSource>().PlayOneShot(hunger1);
+            }
+
+        }
+        else if (MazeController.FullMeter > 0f && MazeController.FullMeter < 0.3f)
+        {
+            if (currentHungerStage != HungerStage.s)
+            {
+                magen.SetTrigger("Jitter");
+                currentHungerStage = HungerStage.s;
+                GetComponent<AudioSource>().PlayOneShot(hunger2);
+            }
+
+        }
+        else if (MazeController.FullMeter == 0f)
+        {
+            if (currentHungerStage != HungerStage.xs)
+            {
+                currentHungerStage = HungerStage.xs;
+                GetComponent<AudioSource>().PlayOneShot(hunger3);
+            }
+
+        }
+
+
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (currentState == UIState.game)
@@ -79,6 +132,7 @@ public class UI_Logic : MonoBehaviour
         Time.timeScale = 0.1f;
         currentState = UIState.endOfGame;
         loseMenu.SetActive(true);
+        GetComponent<AudioSource>().PlayOneShot(hunger3);
     }
 
     public void GoToMenu()
@@ -96,6 +150,7 @@ public class UI_Logic : MonoBehaviour
 
     public void RestartGame()
     {
+        MazeController.FullMeter = 1f;
         Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
